ef02c6aef64f6c87bf0d376419bcdb40ce361459
Codi Kraus <codi.kraus@gmail.com>
2021-04-25 22:10:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,6 +37,24 @@ def valid_move?(board, index)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |turncounter|
+    if turncounter == "X" || turncounter == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  elsif turn_count(board).odd?
+    return "O"
+  end
+end
+
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
