3ae879dacf51ef9a10789f6d98c689ffd07812ea
Codi Kraus <codi.kraus@gmail.com>
2021-04-25 22:14:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,10 +92,8 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board)
-    return false
-  elsif won?(board)
-    return false
+  else
+    false
   end
 end
 
